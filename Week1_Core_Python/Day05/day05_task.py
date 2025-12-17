7a905d9329002d7c54bf3e931b75663ae7ed5b35
Suleyman <suleymanmalikov03@gmail.com>
2025-12-17 11:47:45 +0100

    finished Day05 of the task and instruction

---
+++ Week1_Core_Python/Day05/day05_task.py
@@ -0,0 +1,10 @@
+grades = [80, 90, 60, 70, 50]
+
+# Max of Grades
+print(f"Maximum grade: {max(grades)}")
+
+# Mind of Grades
+print(f"Minimum grade: {min(grades)}")
+
+average = sum(grades)/len(grades)
+print(f"Average of grades {average}")
